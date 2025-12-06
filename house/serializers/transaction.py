2d56043c1efb2c65c7915bf782c9bdd01a39f70e
Qahramon <qbegboyev@gmail.com>
2025-12-06 10:59:01 +0500

    update transaction

--- house/serializers/transaction.py
+++ house/serializers/transaction.py
@@ -6,4 +6,5 @@ from house.models import Transactions
 class TransactionModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transactions
-        fields = ('name','category','description','price','status',)
+        fields = ('id', 'name', 'category', 'description', 'price', 'status', 'created_at')
+        read_only_fields = ('created_at', 'id',)
